df405a4f4627e9f1f37097a8c656ce43cafa69b6
SaintShit <saintshit@proton.me>
2023-04-03 22:59:16 +0330

    fix get-marzban.sh

--- get-marzban.sh
+++ get-marzban.sh
@@ -1,6 +1,34 @@
+#!/usr/bin/env sh
+
+colorized_echo() {
+    local color=$1
+    local text=$2
+    
+    case $color in
+        "red")
+        echo -e "\e[91m${text}\e[0m";;
+        "green")
+        echo -e "\e[92m${text}\e[0m";;
+        "yellow")
+        echo -e "\e[93m${text}\e[0m";;
+        "blue")
+        echo -e "\e[94m${text}\e[0m";;
+        "magenta")
+        echo -e "\e[95m${text}\e[0m";;
+        "cyan")
+        echo -e "\e[96m${text}\e[0m";;
+        *)
+            echo "${text}"
+        ;;
+    esac
+}
+
 if [ "$(id -u)" != "0" ]; then
-    echo -e "\e[91mThis script must be run as root.\e[0m"
+    colorized_echo red "This script must be run as root"
     exit 1
 fi
-curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh -o /usr/local/bin/marzban.sh
+
+colorized_echo blue "Installing marzban.sh"
+curl -L https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh -o /usr/local/bin/marzban.sh
 chmod +x /usr/local/bin/marzban.sh
+colorized_echo green "marzban.sh installed successfully"
